3a66900e65ed48ca09dbe7e7886eb08f0dc0b472
Michael Ringenberg <ringenberg@cmu.edu>
2018-12-04 15:24:35 -0500

    Adds some hardening to tagging task so that it should retry on mysql errors and adds a small delay between tasks to see if concurrency is a problem.

--- app/tasks.py
+++ app/tasks.py
@@ -11,7 +11,7 @@ import MSWord
 import db
 
 celery = create_celery_app()
-#LOGGER = logging #get_task_logger(__name__)
+#LOGGER = get_task_logger(__name__)
 
 def get_dictionary(dictionary="default"):
     """Retrieve the dictionary."""
@@ -89,8 +89,8 @@ def session_scope():
     finally:
         session.close()
 
-@celery.task
-def tag_entry(doc_id):
+@celery.task(bind=True, default_retry_delay=5*60, max_retries=5, rate_limit=1) # retry in 5 minutes and limit 1/s to minimize collisions.
+def tag_entry(self, doc_id):
     """Uses DocuScope tagger on the document stored in a database.
     Arguments:
     doc_id: a string and is the id of the document in the database."""
@@ -98,23 +98,27 @@ def tag_entry(doc_id):
     ds_dict = "default"
     doc_processed = "ERROR: No file data to process."
     doc_state = "3"
-    with session_scope() as session:
-        #session = celery.app.Session()
-        doc = session.query(db.Filesystem).filter_by(id=doc_id).first()
-        #TODO: if not doc: throw
-        if doc:
-            doc.state = "1"
-        session.commit() # Update state
-
-        # Get dictionary
-        assignment = session.query(db.Assignment).filter_by(id=doc.assignment).first()
-        #TODO: if not assignment: throw?
-        if assignment:
-            ds_dict = assignment.dictionary
-        else:
-            print("Bad Assignment, no dictionary specified, using default")
-        doc_json = doc.json
-    # Do processing outside of session_scope as it is very long.
+    try:
+        with session_scope() as session:
+            #session = celery.app.Session()
+            doc = session.query(db.Filesystem).filter_by(id=doc_id).first()
+            #TODO: if not doc: throw
+            if doc:
+                # Get dictionary
+                assignment = session.query(db.Assignment).filter_by(id=doc.assignment).first()
+                #TODO: if not assignment: throw?
+                if assignment:
+                    ds_dict = assignment.dictionary
+                else:
+                    print("Bad Assignment, no dictionary specified, using default")
+                doc_json = doc.json
+                doc.state = "1"
+                #session.commit() # Update state
+            else:
+                print("Could not load {}!".format(doc_id))
+    except Exception as exc:
+        raise self.retry(exc=exc) # most likely a mysql network error and hopefully a delay will fix it.
+        # Do processing outside of session_scope as it is very long.
     if doc_json:
         try:
             doc_dict = create_tag_dict(MSWord.toTOML(doc_json), ds_dict)
@@ -122,13 +126,21 @@ def tag_entry(doc_id):
             #TODO: check for errors
             doc_processed = json.dumps(doc_dict)
             doc_state = "2"
-        except:
+        except Exception as exc:
+            print(exc)
             doc_processed = "{0}".format(sys.exc_info())
             doc_state = "3"
+            # no retry as this will likely be an unrecoverable error.
             raise
-    with session_scope() as session:
-        doc = session.query(db.Filesystem).filter_by(id=doc_id).first()
-        doc.processed = doc_processed
-        doc.state = doc_state
+    try:
+        with session_scope() as session:
+            doc = session.query(db.Filesystem).filter_by(id=doc_id).first()
+            if doc:
+                doc.processed = doc_processed
+                doc.state = doc_state
+            else:
+                print("Could not update {} (state={})!".format(doc_id, doc_state))
+    except Exception as exc:
+        raise self.retry(exc=exc)
 if __name__ == '__main__':
     celery.start()
