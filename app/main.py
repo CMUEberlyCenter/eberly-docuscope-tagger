ac0725a32c6359687ce8bdd218094aae36654aef
Michael Ringenberg <mringenb@andrew.cmu.edu>
2022-07-01 12:40:49 -0400

    Removes /test and /profile routes.
    These were for development and testing purposes and are no longer used.

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
 """ The online DocuScope tagger interface. """
 import asyncio
-import cProfile
 import logging
 import re
 import traceback
 from collections import defaultdict
 from datetime import datetime, timedelta, timezone
-from difflib import ndiff
 from time import perf_counter
-from typing import Counter, Dict, List, Literal, Optional
+from typing import Counter, List, Literal, Optional
 from uuid import UUID, uuid1
 
 import emcache
@@ -16,7 +14,6 @@ from bs4 import BeautifulSoup
 from fastapi import Depends, FastAPI, HTTPException, Request, status
 from fastapi.staticfiles import StaticFiles
 #from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
-from jsondiff import diff
 from neo4j import AsyncGraphDatabase
 from neo4j import AsyncSession as NeoAsyncSession
 from pydantic import BaseModel, constr
@@ -32,7 +29,7 @@ from .default_settings import SETTINGS, SQLALCHEMY_DATABASE_URI
 from .docx_to_text import docx_to_text
 from .ds_tagger import get_wordclasses
 from .ity.formatters.simple_html_formatter import SimpleHTMLFormatter
-from .ity.tagger import ItyTaggerResult, neo_tagger, tag_json
+from .ity.tagger import ItyTaggerResult, tag_json
 from .ity.taggers.docuscope_tagger_neo import DocuscopeTaggerNeo
 from .ity.tokenizers.regex_tokenizer import RegexTokenizer
 from .ity.tokenizers.tokenizer import TokenType
@@ -416,132 +413,6 @@ async def status_all(sql: AsyncSession = Depends(session)) -> list[Status]:
                                .group_by(Submission.state))
     return result.all()
 
-
-class CheckResults(BaseModel):
-    """Model for testing results."""
-    #pylint: disable=too-few-public-methods
-    output_check: bool
-    token_check: bool
-    tag_dict_check: int
-
-
-@app.get("/test/{uuid}", response_model=CheckResults)
-async def test_tagging(uuid: UUID,
-                       sql: AsyncSession = Depends(session),
-                       neo: NeoAsyncSession = Depends(neo_session)):
-    """ Check the document status and start the tagging if appropriate. """
-    # check if uuid
-    try:
-        UUID(f"{uuid}")
-    except ValueError as vexc:
-        raise HTTPException(detail=f"{vexc}: {uuid}",
-                            status_code=status.HTTP_400_BAD_REQUEST) from vexc
-    # get current status.
-    query = await sql.execute(select(Submission.state).where(Submission.id == uuid))
-    (state,) = query.first()
-    if state in ['pending', 'submitted']:
-        return {"message": f"{uuid} is not yet tagged."}
-    if state == 'tagged':
-        # check for too many submitted
-        return await check_tagging(uuid, sql, neo, CACHE)
-
-    if state == 'error':
-        raise HTTPException(detail=f"Tagging failed for {uuid}",
-                            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    if state is None:
-        raise HTTPException(detail="Document unavailable.",
-                            status_code=status.HTTP_404_NOT_FOUND)
-    raise HTTPException(detail=f"Unknown state: {state} for {uuid}",
-                        status_code=status.HTTP_503_SERVICE_UNAVAILABLE)
-
-
-@app.get("/profile/{uuid}")
-async def profile_tagging(
-        uuid: UUID,
-        sql: AsyncSession = Depends(session),
-        neo: NeoAsyncSession = Depends(neo_session)):
-    """Profile tagging and dump to file."""
-    query = await sql.execute(select(Submission.content).where(Submission.id == uuid))
-    (doc_content,) = query.first()
-    tagger = neo_tagger(WORDCLASSES, neo)
-    content = docx_to_text(doc_content)
-    with cProfile.Profile() as prof:
-        tagger.tag(content)
-    prof.dump_stats('mprof.stat')
-
-
-@app.post('/batchtest', response_model=Dict[UUID, CheckResults])
-async def test_corpus(
-        corpus: list[UUID],
-        sql: AsyncSession = Depends(session),
-        neo: NeoAsyncSession = Depends(neo_session)):
-    """Perform tagging check on multiple documents."""
-    return {uuid: await check_tagging(uuid, sql, neo, CACHE) for uuid in corpus}
-
-
-async def check_tagging(  # pylint: disable=too-many-locals
-        doc_id: UUID,
-        sql: AsyncSession,
-        neo: NeoAsyncSession,
-        cache: emcache.Client) -> CheckResults:
-    """Perform tagging with neo4j tagger and check against the database. """
-    query = await sql.execute(select(Submission.content, Submission.name, Submission.processed)
-                              .where(Submission.id == doc_id))
-    (doc_content, name, processed) = query.first() or (None, None, None)
-    if doc_content:
-        try:
-            if name.endswith(".docx"):
-                doc_content = docx_to_text(doc_content)
-            tokenizer = RegexTokenizer()
-            tokens = tokenizer.tokenize(doc_content)
-            tagger = DocuscopeTaggerNeo(return_untagged_tags=False, return_no_rules_tags=True,
-                                        return_included_tags=True, wordclasses=WORDCLASSES,
-                                        session=neo, cache=cache)
-            tagger_gen = tagger.tag_next(tokens)
-            while True:
-                try:
-                    await tagger_gen.asend(None)
-                except StopAsyncIteration:
-                    break
-            output = SimpleHTMLFormatter().format(tags=(tagger.rules, tagger.tags),
-                                                  tokens=tokens, text_str=doc_content)
-        except Exception as exc:
-            logging.error("Error while tagging %s", doc_id)
-            traceback.print_exc()
-            raise exc
-        type_count = Counter([token.type for token in tokens])
-        not_excluded = set(TokenType) - set(tokenizer.excluded_token_types)
-        test_processed = tag_json(ItyTaggerResult(
-            text_contents=doc_content,
-            format_output=output,
-            tag_dict=tagger.rules,
-            num_tokens=len(tokens),
-            num_word_tokens=type_count[TokenType.WORD],
-            num_punctuation_tokens=type_count[TokenType.PUNCTUATION],
-            num_included_tokens=sum(type_count[itype]
-                                    for itype in not_excluded),
-            num_excluded_tokens=sum(type_count[etype]
-                                    for etype in tokenizer.excluded_token_types),
-            tag_chain=[tag.rules[0][0].split('.')[-1] for tag in tagger.tags]
-        )).dict()
-        tag_diff = diff(processed, test_processed)
-        if tag_diff:
-            logging.warning("diff: %s", tag_diff)
-        if processed['ds_output'] != test_processed['ds_output']:
-            logging.warning(''.join(ndiff(
-                "</span>\n".join(
-                    processed['ds_output'].split('</span>')).splitlines(keepends=True),
-                "</span>\n".join(
-                    test_processed['ds_output'].split("</span>")).splitlines(keepends=True))))
-        return {
-            "output_check": processed['ds_output'] == test_processed['ds_output'],
-            "token_check": processed['ds_num_tokens'] == test_processed['ds_num_tokens'],
-            "tag_dict_check": len(processed['ds_tag_dict']) - len(test_processed['ds_tag_dict']),
-            # "tag_count": str(diff(doc_processed['ds_count_dict'], processed['ds_count_dict']))
-        }
-    logging.error("No document content for %s.", doc_id)
-    raise HTTPException(detail=f"No document content for {doc_id}.",
-                        status_code=status.HTTP_404_NOT_FOUND)
 
 app.mount('/static', StaticFiles(directory="app/static", html=True))
 
