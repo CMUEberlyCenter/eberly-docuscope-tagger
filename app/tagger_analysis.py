6935f1c4981c8d6b550f0b6b191e4fbd1ae42214
Michael Ringenberg <mringenb@andrew.cmu.edu>
2022-12-16 09:09:30 -0500

    chore: pylint fixes.

--- app/tagger_analysis.py
+++ app/tagger_analysis.py
@@ -1,49 +1,65 @@
+"""
+Basic data analysis functions.
+
+A collection of functions that produce various summary statistics over the collected
+tagging data.
+"""
 import asyncio
 from collections import Counter
 import json
 
-from pandas import DataFrame, Interval, Timestamp, melt
+from pandas import DataFrame, Interval, Timestamp
 from pandas.arrays import IntervalArray
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-from .database import Submission, Tagging
+from .database import Tagging
 from .default_settings import SQLALCHEMY_DATABASE_URI
 
 
 async def tagger_performance(sql: AsyncSession):
+    """Prints out performance statistics for the online DocuScope tagger."""
     # TODO add time constraints (eg) WHERE TIMESTAMP('2022-09-26') <= started
     query = await sql.execute(select(Tagging).order_by(Tagging.started))
-    df = DataFrame([[data.state,
-                     data.word_count,
-                     Interval(left=Timestamp(data.started), right=Timestamp(data.finished), closed='both')]
-                    for data in query.scalars()],
+    data = DataFrame([[
+            data.state,
+            data.word_count,
+            Interval(left=Timestamp(data.started), right=Timestamp(data.finished), closed='both')]
+          for data in query.scalars()],
         columns=['state', 'word_count', 'interval'])
-    #print(df)
+    # print(df)
     print('--- States ---')
-    print(df['state'].value_counts())
+    print(data['state'].value_counts())
     print('--- TimeDelta ---')
-    df['delta'] = df['interval'].apply(lambda i: i.length.total_seconds())
+    data['delta'] = data['interval'].apply(lambda i: i.length.total_seconds())
     #print(df['interval'].apply(lambda i: i.length).describe())
-    print(df['delta'].describe())
+    print(data['delta'].describe())
     print('--- Word Count ---')
-    print(df['word_count'].describe())
+    print(data['word_count'].describe())
     print('--- Words/sec ---')
-    df['words_per_second'] = df['word_count'] / df['delta']
-    print(df['words_per_second'].describe())
+    data['words_per_second'] = data['word_count'] / data['delta']
+    print(data['words_per_second'].describe())
     #out = df['count'].describe() + df['delta'] + df['wc_delta']
     out = {}
-    out['describe'] = df.describe().to_dict()
-    out['state'] = df['state'].value_counts().to_dict()
+    out['describe'] = data.describe().to_dict()
+    out['state'] = data['state'].value_counts().to_dict()
 
-    intervals = IntervalArray(df['interval'])
-    df['overlap'] = (df.loc[:, 'interval']).apply(lambda i: intervals.overlaps(i).sum()-1)
-    out['overlaps'] = df['overlap'].value_counts().tolist()
-    #print(json.dumps(out))
+    intervals = IntervalArray(data['interval'])
+    data['overlap'] = (data.loc[:, 'interval']).apply(
+        lambda i: intervals.overlaps(i).sum()-1)
+    out['overlaps'] = data['overlap'].value_counts().tolist()
+    print(json.dumps(out))
 
-#async def classroom_performance(sql: AsyncSession):
-#    query = await sql.execute(select(Submission.assignment, Submission.owner, Submission.created, Submission.fullname, Submission.state, Submission.ownedby, Submission.processed))
+# async def classroom_performance(sql: AsyncSession):
+#    query = await sql.execute(select(
+# Submission.assignment,
+#  Submission.owner,
+#  Submission.created,
+#  Submission.fullname,
+#  Submission.state,
+#  Submission.ownedby,
+#  Submission.processed))
 #    df = DataFrame([[
 #        data.state,
 #        data.assignment,
@@ -54,7 +70,9 @@ async def tagger_performance(sql: AsyncSession):
 #    ] for data in query.scalars()])
 #    print(df)
 
+
 async def tagger_analysis(sql: AsyncSession):
+    """Prints out various pattern and category counts."""
     tagging = await sql.stream_scalars(select(Tagging).where(Tagging.state == 'success'))
     patterns = Counter()
     categories = Counter()
@@ -62,24 +80,28 @@ async def tagger_analysis(sql: AsyncSession):
         data = row.detail
         for category in data['patterns']:
             if category['category'] != '?':
-                cat_count = Counter({pat['pattern']: pat['count'] for pat in category['patterns']})
+                cat_count = Counter({pat['pattern']: pat['count']
+                                    for pat in category['patterns']})
                 patterns.update(cat_count)
                 categories.update({category['category']: cat_count.total()})
     print(f"10 Most common patterns: {patterns.most_common(10)}")
     if len(patterns) > 0:
-        longest = max(patterns.keys(), key = lambda i: len(i.split(' ')))
+        longest = max(patterns.keys(), key=lambda i: len(i.split(' ')))
         print(f'Longest pattern: "{longest}" count: {patterns[longest]}')
     print(f"Category frequency: {categories.most_common()}")
     return
 
+
 async def async_main():
+    """Main function for running the various metrics asyncroniously."""
     engine = create_async_engine(SQLALCHEMY_DATABASE_URI)
-    async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+    async_session = sessionmaker(
+        engine, expire_on_commit=False, class_=AsyncSession)
     async with async_session() as session:
         await tagger_performance(session)
-        #await classroom_performance(session)
+        # await classroom_performance(session)
         await tagger_analysis(session)
     await engine.dispose()
- 
+
 if __name__ == "__main__":
     asyncio.run(async_main())
