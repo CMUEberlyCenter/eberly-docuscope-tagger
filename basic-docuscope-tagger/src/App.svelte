1ac06c60128f232f81acbf39a016de60b2d1a8b2
Michael Ringenberg <mringenb@andrew.cmu.edu>
2023-08-03 15:15:13 -0400

    [enhance] adds lightswitch
    basic app can now switch to dark mode.

--- basic-docuscope-tagger/src/App.svelte
+++ basic-docuscope-tagger/src/App.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-  import { AppBar } from "@skeletonlabs/skeleton";
+  import { AppBar, LightSwitch } from "@skeletonlabs/skeleton";
   //import Statistics from './lib/Statistics.svelte';
   import Tagger from "./lib/Tagger.svelte";
 </script>
 
-<AppBar background="bg-primary-500">
+<AppBar background="bg-primary-500" slotTrail="place-content-end">
   <a
     href="https://www.cmu.edu/dietrich/english/research/docuscope.html"
     class="text-white font-bold text-lg"
   >
     DocuScope
   </a>
+  <svelte:fragment slot="trail"><LightSwitch/></svelte:fragment>
 </AppBar>
 <Tagger />
