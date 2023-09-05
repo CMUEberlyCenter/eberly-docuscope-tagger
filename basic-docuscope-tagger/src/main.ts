3b247173a0af257f9dac2db9a38173b77a3867c1
Michael Ringenberg <mringenb@andrew.cmu.edu>
2023-09-05 13:24:08 -0400

    [dependency] upgrade to skeleton 2

--- basic-docuscope-tagger/src/main.ts
+++ basic-docuscope-tagger/src/main.ts
@@ -1,13 +1,9 @@
-// Your selected Skeleton theme:
-import "@skeletonlabs/skeleton/themes/theme-skeleton.css";
-// This contains the bulk of Skeletons required styles:
-import "@skeletonlabs/skeleton/styles/skeleton.css";
-// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+import "./app.postcss";
 import "./app.scss";
 import App from "./App.svelte";
 
 const app = new App({
-  target: document.getElementById("app"),
+  target: document.getElementById("app")!,
 });
 
 export default app;
